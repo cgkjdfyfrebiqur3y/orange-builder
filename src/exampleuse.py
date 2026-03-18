a47aaf1943d40a1376b0dc602fa9ebfa1d574446
cgkjdfyfrebiqur3y <131993539+cgkjdfyfrebiqur3y@users.noreply.github.com>
2026-03-18 16:50:50 +0100

    Delete src directory

--- src/exampleuse.py
+++
@@ -1,4 +0,0 @@
-import orange_generator
-
-while True:
-    print(next(orange_generator.RainbowOrangeGenerator()))
--- src/helpers/ifexists.py
+++
@@ -1,7 +0,0 @@
-class Ifexists:
-    def __init__(self, value, default):
-        self.value = value
-        self.default = default
-
-    def __call__(self):
-        return self.default if not self.value else self.value
